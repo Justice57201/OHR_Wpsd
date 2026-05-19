7cdf88180cbd8cae649666f4b7e061fd83befb4a
Justice <60824609+Justice57201@users.noreply.github.com>
2026-05-19 17:25:49 -0500

    Update HostFilesUpdate.sh

--- HostFilesUpdate.sh
+++ HostFilesUpdate.sh
@@ -18,22 +18,22 @@ if [ "$(expr length `hostname -I | cut -d' ' -f1`x)" == "1" ]; then
 fi
 
 APRSHOSTS=/usr/local/etc/APRSHosts.txt
-DCSHOSTS=/usr/local/etc/DCS_Hosts.txt
-DExtraHOSTS=/usr/local/etc/DExtra_Hosts.txt
+# DCSHOSTS=/usr/local/etc/DCS_Hosts.txt
+# DExtraHOSTS=/usr/local/etc/DExtra_Hosts.txt
 DMRIDFILE=/usr/local/etc/DMRIds.dat
 DMRHOSTS=/usr/local/etc/DMR_Hosts.txt
-DPlusHOSTS=/usr/local/etc/DPlus_Hosts.txt
+# DPlusHOSTS=/usr/local/etc/DPlus_Hosts.txt
 P25HOSTS=/usr/local/etc/P25Hosts.txt
-M17HOSTS=/usr/local/etc/M17Hosts.txt
-YSFHOSTS=/usr/local/etc/YSFHosts.txt
-FCSHOSTS=/usr/local/etc/FCSHosts.txt
-XLXHOSTS=/usr/local/etc/XLXHosts.txt
+# M17HOSTS=/usr/local/etc/M17Hosts.txt
+# YSFHOSTS=/usr/local/etc/YSFHosts.txt
+# FCSHOSTS=/usr/local/etc/FCSHosts.txt
+# XLXHOSTS=/usr/local/etc/XLXHosts.txt
 NXDNIDFILE=/usr/local/etc/NXDN.csv
 NXDNHOSTS=/usr/local/etc/NXDNHosts.txt
-TGLISTBM=/usr/local/etc/TGList_BM.txt
+# TGLISTBM=/usr/local/etc/TGList_BM.txt
 TGLISTP25=/usr/local/etc/TGList_P25.txt
 TGLISTNXDN=/usr/local/etc/TGList_NXDN.txt
-TGLISTYSF=/usr/local/etc/TGList_YSF.txt
+# TGLISTYSF=/usr/local/etc/TGList_YSF.txt
 NEXTIONGROUPS=/usr/local/etc/nextionGroups.txt
 NEXTIONUSERS=/usr/local/etc/nextionUsers.csv
 USERCSV=/usr/local/etc/user.csv
@@ -50,22 +50,22 @@ fi
 # Create backup of old files
 if [ ${FILEBACKUP} -ne 0 ]; then
 	cp ${APRSHOSTS} ${APRSHOSTS}.$(date +%Y%m%d)
-	cp ${DCSHOSTS} ${DCSHOSTS}.$(date +%Y%m%d)
-	cp ${DExtraHOSTS} ${DExtraHOSTS}.$(date +%Y%m%d)
+	# cp ${DCSHOSTS} ${DCSHOSTS}.$(date +%Y%m%d)
+	# cp ${DExtraHOSTS} ${DExtraHOSTS}.$(date +%Y%m%d)
 	cp ${DMRIDFILE} ${DMRIDFILE}.$(date +%Y%m%d)
 	cp ${DMRHOSTS} ${DMRHOSTS}.$(date +%Y%m%d)
-	cp ${DPlusHOSTS} ${DPlusHOSTS}.$(date +%Y%m%d)
+	# cp ${DPlusHOSTS} ${DPlusHOSTS}.$(date +%Y%m%d)
 	cp ${P25HOSTS} ${P25HOSTS}.$(date +%Y%m%d)
-	cp ${M17HOSTS} ${M17HOSTS}.$(date +%Y%m%d)
-	cp ${YSFHOSTS} ${YSFHOSTS}.$(date +%Y%m%d)
-	cp ${FCSHOSTS} ${FCSHOSTS}.$(date +%Y%m%d)
-	cp ${XLXHOSTS} ${XLXHOSTS}.$(date +%Y%m%d)
+	# cp ${M17HOSTS} ${M17HOSTS}.$(date +%Y%m%d)
+	# cp ${YSFHOSTS} ${YSFHOSTS}.$(date +%Y%m%d)
+	# cp ${FCSHOSTS} ${FCSHOSTS}.$(date +%Y%m%d)
+	# cp ${XLXHOSTS} ${XLXHOSTS}.$(date +%Y%m%d)
 	cp ${NXDNIDFILE} ${NXDNIDFILE}.$(date +%Y%m%d)
 	cp ${NXDNHOSTS} ${NXDNHOSTS}.$(date +%Y%m%d)
-	cp ${TGLISTBM} ${TGLISTBM}.$(date +%Y%m%d)
+	# cp ${TGLISTBM} ${TGLISTBM}.$(date +%Y%m%d)
 	cp ${TGLISTP25} ${TGLISTP25}.$(date +%Y%m%d)
 	cp ${TGLISTNXDN} ${TGLISTNXDN}.$(date +%Y%m%d)
-	cp ${TGLISTYSF} ${TGLISTYSF}.$(date +%Y%m%d)
+	# cp ${TGLISTYSF} ${TGLISTYSF}.$(date +%Y%m%d)
 fi
 
 # Prune backups
@@ -101,32 +101,32 @@ done
 
 # Generate Host Files
 curl --fail -o ${APRSHOSTS} -s https://www.gmrs-link.com/ohr/mmdvm/APRS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${DCSHOSTS} -s http://www.pistar.uk/downloads/DCS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# curl --fail -o ${DCSHOSTS} -s http://www.pistar.uk/downloads/DCS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 curl --fail -o ${DMRHOSTS} -s https://www.gmrs-link.com/ohr/mmdvm/DMR_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 
-if [ -f /etc/hostfiles.nodextra ]; then
-  curl --fail -o ${DPlusHOSTS} -s http://www.pistar.uk/downloads/DPlus_WithXRF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-  curl --fail -o ${DExtraHOSTS} -s http://www.pistar.uk/downloads/DExtra_NoXRF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-else
-  curl --fail -o ${DPlusHOSTS} -s http://www.pistar.uk/downloads/DPlus_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-  curl --fail -o ${DExtraHOSTS} -s http://www.pistar.uk/downloads/DExtra_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-fi
+# if [ -f /etc/hostfiles.nodextra ]; then
+#  curl --fail -o ${DPlusHOSTS} -s http://www.pistar.uk/downloads/DPlus_WithXRF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+#  curl --fail -o ${DExtraHOSTS} -s http://www.pistar.uk/downloads/DExtra_NoXRF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# else
+#  curl --fail -o ${DPlusHOSTS} -s http://www.pistar.uk/downloads/DPlus_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+#  curl --fail -o ${DExtraHOSTS} -s http://www.pistar.uk/downloads/DExtra_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# fi
 
 curl -sSL https://www.gmrs-link.com/ohr/mmdvm/DMRIds.dat.gz --user-agent "Pi-Star_${pistarCurVersion}" | gunzip -c > ${DMRIDFILE}
 curl -sSL https://www.gmrs-link.com/ohr/mmdvm/nextionUsers.csv --user-agent "Pi-Star_${pistarCurVersion}" -o "${NEXTIONUSERS}"
 curl -sSL https://www.gmrs-link.com/ohr/hblink/user.csv --user-agent "Pi-Star_${pistarCurVersion}" -o "${USERCSV}"
 
 curl --fail -o ${P25HOSTS} -s https://www.gmrs-link.com/ohr/mmdvm/P25_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${M17HOSTS} -s http://www.pistar.uk/downloads/M17_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${YSFHOSTS} -s http://www.pistar.uk/downloads/YSF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${FCSHOSTS} -s http://www.pistar.uk/downloads/FCS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${XLXHOSTS} -s http://www.pistar.uk/downloads/XLXHosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# curl --fail -o ${M17HOSTS} -s http://www.pistar.uk/downloads/M17_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# curl --fail -o ${YSFHOSTS} -s http://www.pistar.uk/downloads/YSF_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# curl --fail -o ${FCSHOSTS} -s http://www.pistar.uk/downloads/FCS_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# curl --fail -o ${XLXHOSTS} -s http://www.pistar.uk/downloads/XLXHosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
 curl --fail -o ${NXDNIDFILE} -s https://www.gmrs-link.com/ohr/mmdvm/NXDN.csv --user-agent "Pi-Star_${pistarCurVersion}"
 curl --fail -o ${NXDNHOSTS} -s https://www.gmrs-link.com/ohr/mmdvm/NXDN_Hosts.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${TGLISTBM} -s http://www.pistar.uk/downloads/TGList_BM.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# curl --fail -o ${TGLISTBM} -s http://www.pistar.uk/downloads/TGList_BM.txt --user-agent "Pi-Star_${pistarCurVersion}"
 curl --fail -o ${TGLISTP25} -s https://www.gmrs-link.com/ohr/mmdvm/TGList_P25.txt --user-agent "Pi-Star_${pistarCurVersion}"
 curl --fail -o ${TGLISTNXDN} -s https://www.gmrs-link.com/ohr/mmdvm/TGList_NXDN.txt --user-agent "Pi-Star_${pistarCurVersion}"
-curl --fail -o ${TGLISTYSF} -s http://www.pistar.uk/downloads/TGList_YSF.txt --user-agent "Pi-Star_${pistarCurVersion}"
+# curl --fail -o ${TGLISTYSF} -s http://www.pistar.uk/downloads/TGList_YSF.txt --user-agent "Pi-Star_${pistarCurVersion}"
 curl --fail -o ${NEXTIONGROUPS} -s https://www.gmrs-link.com/ohr/mmdvm/groups.txt --user-agent "Pi-Star_${pistarCurVersion}"
 
 # Overrides
@@ -134,13 +134,13 @@ if [ -f "/root/DMR_Hosts.txt" ]; then
 	cat /root/DMR_Hosts.txt >> ${DMRHOSTS}
 fi
 
-if [ -f "/root/YSFHosts.txt" ]; then
-	cat /root/YSFHosts.txt >> ${YSFHOSTS}
-fi
+# if [ -f "/root/YSFHosts.txt" ]; then
+#	cat /root/YSFHosts.txt >> ${YSFHOSTS}
+# fi
 
-if [ -f "/root/M17Hosts.txt" ]; then
-	cat /root/M17Hosts.txt >> ${M17HOSTS}
-fi
+# if [ -f "/root/M17Hosts.txt" ]; then
+#	cat /root/M17Hosts.txt >> ${M17HOSTS}
+# fi
 
 if [ -f "/root/NXDNHosts.txt" ]; then
 	cat /root/NXDNHosts.txt > /usr/local/etc/NXDNHostsLocal.txt
